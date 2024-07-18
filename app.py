f9da8d4928d1bded08aafabf417436b680e5d829
Borjaja2000 <46570584+Borjaja2000@users.noreply.github.com>
2024-07-18 10:31:52 +0200

    Initial Commit

--- app.py
+++ app.py
@@ -10,8 +10,9 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import toml
 import streamlit.components.v1 as components
 
+
 logo = 'imagenes/spotify.png'
-st.set_page_config(page_title="Spotify", page_icon=logo ,layout="wide") #configuración de la página
+st.set_page_config(page_title="Spotify", page_icon=logo ,layout="wide") #Configuración de la página
 #Funciones
 @st.cache_data(ttl=3600)
 def cargar_datos():
@@ -19,7 +20,19 @@ def cargar_datos():
     return df
 
 @st.cache_data(ttl=3600)
-def calcular_top_artistas(df, numero_artistas):
+# 
+def calcular_top_artistas(df, numero_artistas: int)->pd.DataFrame:
+    """
+    Función para calcular top n artistas de una canción con la media
+    mas alta de popularidad.
+    
+    Args:
+    - df (pd.Dataframe): dataframe 
+    - numero_artistas(int): numero de artistas a mostrar
+    
+    Returns:
+    - top_n_artists (pd.Dataframe): dataframe filtrado con los artistas
+    """
     artist_info = df.groupby('artist_name').agg({
         'popularity': 'mean',
         'genre': 'first'  # Tomar el primer género encontrado para cada artista
@@ -35,7 +48,17 @@ def ordenar_por_popularidad(df, top):
     return df
 
 @st.cache_data(ttl=3600)
-def artistas_con_mas_canciones(df, numero_artistas):
+def artistas_con_mas_canciones(df, numero_artistas: int)->pd.DataFrame:
+    """
+    Función para calcular los artistas con más canciones en el dataset.
+
+    Args:
+    - df (pd.Dataframe): dataframe
+    - numero_artistas (int): número de artistas a mostrar
+
+    Returns:
+    - top_50_artists (pd.Dataframe): dataframe con los artistas con más canciones
+    """
     df_aux = df
     songs_per_artist = df_aux.groupby('artist_name', as_index=False)['track_name'].count()
     songs_per_artist.rename(columns={'track_name': 'song_count'}, inplace=True)
@@ -48,9 +71,17 @@ def cargar_html(ruta_archivo):
         html_content = f.read()
     return html_content
 
-config = toml.load(".streamlit/config.toml")
-
 def clean_outliers(df_aux, columns: list)->pd.DataFrame:
+    """
+    Función para eliminar los outliers de un dataframe usando el rango intercuartílico.
+    
+    Args:
+    - df_aux (pd.Dataframe): dataframe a limpiar
+    - columns(list): lista de columnas a limpiar
+    
+    Returns:
+    - df_aux (pd.Dataframe): dataframe sin outliers
+    """
     for column in columns:
         Q1 = df_aux[column].quantile(0.25)
         Q3 = df_aux[column].quantile(0.75)
@@ -60,7 +91,7 @@ def clean_outliers(df_aux, columns: list)->pd.DataFrame:
 
 df = cargar_datos()
 
-# Centrar el título
+# Centrar el título de la página
 st.markdown(
     """
     <style>
@@ -72,8 +103,10 @@ st.markdown(
     unsafe_allow_html=True
 )
 
+# Título de la página
 st.markdown("<h1 class='title'>Análisis de canciones en Spotify</h1>", unsafe_allow_html=True)
 st.divider()
+
 # Icono de Spotify
 st.sidebar.markdown("""
     <img src="https://cdn.icon-icons.com/icons2/3041/PNG/512/spotify_logo_icon_189218.png" 
@@ -112,18 +145,22 @@ if pestaña == "Inicio":
 elif pestaña == "Distribución variables":
     tabsInicio = st.tabs(["Variables continuas", "Correlación de Spearman"])
     with tabsInicio[0]:
+        # Distribución de las variables continuas
         st.image('imagenes/distribucion.png')
     with tabsInicio[1]:
+        # Correlación de Spearman
         st.image('imagenes/spearman.png')
 
 elif pestaña == "Popularidad":
     st.sidebar.divider()
     st.sidebar.markdown("### Configuración")
+    # Número de artistas y canciones a mostrar en las gráficas interactivas
     numero_artistas = st.sidebar.slider("Número de artistas", 1, 50, 10, key="artistas")
     numero_canciones = st.sidebar.slider("Número de canciones", 1, 50, 10, key="canciones")
 
     tabsPopularidad = st.tabs([f"Top Artistas y Canciones", "Bailable", "Género", "Energía", "Positividad"])
     with tabsPopularidad[0]:
+        # Gráfica top n artistas con la media más alta de popularidad
         top_n_artists = calcular_top_artistas(df, numero_artistas)
         fig = px.treemap(top_n_artists,  
                         path=['artist_name'], 
@@ -163,22 +200,23 @@ elif pestaña == "Popularidad":
     with tabsPopularidad[2]:
         # Histograma de popularidad media por género y año
         html_content = cargar_html("html/popularidadgeneros.html")
-        st.components.v1.html(html_content, height=700)
+        components.html(html_content, height=700)
     with tabsPopularidad[3]:
+        # Histograma de la energía media en base a la popularidad segun el año
         html_content = cargar_html("html/energiapopularidad.html")
-        st.components.v1.html(html_content, height=700)
+        components.html(html_content, height=700)
     with tabsPopularidad[4]:
-        # Histograma de media de la positividad en base a la popularidad segun el año
+        # Histograma de la positividad media en base a la popularidad segun el año
         html_content = cargar_html("html/positividadpopularidad.html")
-        st.components.v1.html(html_content, height=700)
+        components.html(html_content, height=700)
 elif pestaña == "Características de la canción":
     st.sidebar.markdown("---")
     st.sidebar.markdown("### Configuración")
+    # Número de artistas a mostrar en la gráfica
     numero_artistas = st.sidebar.slider("Número de artistas", 1, 50, 10, key="artistas")
     tabsCaracteristicas = st.tabs(["Artistas", "Volumen", "Tempo"])
     with tabsCaracteristicas[0]:
         # Grafico artistas con mas canciones
-        
         top_50_artists = artistas_con_mas_canciones(df, numero_artistas)
 
         fig = px.treemap(top_50_artists, 
@@ -191,18 +229,18 @@ elif pestaña == "Características de la canción":
         fig.update_traces(hovertemplate='Artista: %{label}<br>Número de Canciones: %{value}')
         st.plotly_chart(fig)
     with tabsCaracteristicas[1]:
-        # Histograma de volumen con la energía promedio
+        # Histograma del volumen con la energía promedio
         with open("html/volumenenergia.html", "r", encoding="utf-8") as f:
             html_content = f.read()
-        st.components.v1.html(html_content, height=700)
+        components.html(html_content, height=700)
 
     with tabsCaracteristicas[2]:
         # Histograma bailabilidad en base al tempo de las canciones
         html_content = cargar_html("html/tempobailable.html")
-        st.components.v1.html(html_content, height=500)
+        components.html(html_content, height=500)
         st.image('imagenes/tempobailable.png')  
 elif pestaña == "Informe":
-    # Power BI
+    # Informe Power BI
     codigo_iframe = '''<iframe title="spotify" width="1320" height="700"
     src="https://app.powerbi.com/view?r=eyJrIjoiMTk5Njg1NjYtYWI5OS00OTg4LTg4OGYtOTE4ZGM3OTFlNWUzIiwidCI6IjhhZWJkZGI2LTM0MTgtNDNhMS1hMjU1LWI5NjQxODZlY2M2NCIsImMiOjl9"
     frameborder="0" allowFullScreen="true"></iframe>'''
@@ -212,6 +250,7 @@ elif pestaña == "Predicción de popularidad":
     st.markdown("### Predicción de la popularidad")
     with st.form(key="form"):
         cols = st.columns(3)
+        # Sliders de las características de la canción para predecir la popularidad
         with cols[0]:
             energy = st.slider("Energía", 0.0, 1.0, 0.5, 0.01, key="energy")
             danceability = st.slider("Bailabilidad", 0.0, 1.0, 0.5, 0.01, key="danceability")
@@ -223,6 +262,7 @@ elif pestaña == "Predicción de popularidad":
         with cols[1]:
             loudness = st.slider("Volumen", -60.0, 0.0, -20.0, 0.01, key="loudness")
             speechiness = st.slider("Discurso", 0.0, 1.0, 0.5, 0.01, key="speechiness")
+            # Cambiamos el color del botón
             m = st.markdown("""
             <style>
             div.stButton > button:first-child {
@@ -230,8 +270,10 @@ elif pestaña == "Predicción de popularidad":
                 margin-top: 10px;
             }
             </style>""", unsafe_allow_html=True)
+            # Botón para predecir la popularidad de la canción
             b = st.form_submit_button("Predecir", use_container_width=True)
             if b:
+                # Código para predecir la popularidad de la canción realizando un modelo de despliegue en Azure Machine Learning
                 def allowSelfSignedHttps(allowed):
                     if allowed and not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None):
                         ssl._create_default_https_context = ssl._create_unverified_context
@@ -286,24 +328,30 @@ elif pestaña == "Predicción de popularidad":
                     print("The request failed with status code: " + str(error.code))
                     print(error.info())
                     print(error.read().decode("utf8", 'ignore'))
-
+    # Usamos las credenciales de cliente para acceder a la API de Spotify
     auth_manager=SpotifyClientCredentials(client_id=st.secrets["spotify"]["client_id"], client_secret=st.secrets["spotify"]["client_secret"], requests_session=True)
+    # Creamos un objeto de la clase Spotify usando las credenciales anteriores
     sp = spotipy.Spotify(auth_manager=auth_manager)
 
+    # Cambiamos el color de los multiselect
     st.markdown("""
     <style>        
     .st-bt.st-ep.st-bv.st-bw.st-bx.st-by.st-ay.st-bs.st-az.st-cr.st-cs.st-ct.st-cu.st-eq.st-er.st-es.st-et.st-au.st-av.st-ax.st-aw.st-cj.st-eu.st-ev.st-ew.st-cl.st-ex.st-ey.st-ba.st-ez.st-f0.st-f1.st-d3.st-f2.st-bb.st-f3 {
-        background-color: #1DB954; /* Cambia este valor al color que desees */
-        color: white; /* Color del texto dentro del tag */
+        background-color: #1DB954;
+        color: white;
     }
     </style>
     """, unsafe_allow_html=True)
 
+    # Multiselect para seleccionar los géneros y hacemos una petición a la API de Spotify para obtener los géneros disponibles
     genres = st.multiselect("Selecciona de uno a tres géneros para obtener canciones similares ", sp.recommendation_genre_seeds()["genres"], max_selections=3)
     parameters_list= ["danceability", "energy", "loudness", "speechiness", "acousticness", "instrumentalness", "valence", "tempo"]
     st.sidebar.markdown("### Parámetros")
+    # Multiselect para seleccionar los parámetros de la canción
     parameters = st.sidebar.multiselect("Seleccione de uno a tres parámetros", parameters_list, max_selections=3)
+
     if genres:
+        # Parámetros para obtener recomendaciones de canciones similares, lo iremos actualizando con los parámetros seleccionados
         args_recomendaciones = {
             "seed_genres": genres, 
             "limit": 4
@@ -359,21 +407,25 @@ elif pestaña == "Predicción de popularidad":
                         "max_tempo": tempo+10, 
                         "target_tempo": tempo
                     })
-                
+        # Obtenemos las recomendaciones de canciones similares haciendo una petición a la API de Spotify        
         results = sp.recommendations(**args_recomendaciones)
 
+        # Mostramos las recomendaciones de canciones similares recorriendo results
         cols = st.columns(4)
         for i in range(len(results["tracks"])):
             with cols[i]:
+                # Obtenemos la URL de Spotify
                 spotify_url = results["tracks"][i]["external_urls"]["spotify"]
                 if spotify_url:
                     if 'track' in spotify_url:
+                        # Obtenemos el ID de la canción
                         track_id = spotify_url.split('/')[-1].split('?')[0]
+                        # Le pasamos el ID de la canción a la URL de Spotify para obtener el embed
                         embed_url = f"https://open.spotify.com/embed/track/{track_id}"
                     else:
                         embed_url = None
 
                     if embed_url:
-                        st.components.v1.iframe(embed_url, width=300, height=380)
+                        components.iframe(embed_url, width=300, height=380)
                     else:
                         st.write("La URL proporcionada no es válida. Asegúrate de que sea una URL de pista o lista de reproducción de Spotify.")
